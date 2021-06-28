8af102f6c4290f549312f615145a6e6dbef84101
Yilmar Ospina <ospina.yilmar@gmail.com>
2021-06-27 21:21:18 -0500

    Primer ejercicio del taller 2 funcionando

---
+++ descuentos.js
@@ -0,0 +1,14 @@
+function calculateDiscount() {
+    const price = document.getElementById('price').value;
+    const discount = document.getElementById('discount').value;
+
+    const priceWithDiscount = getPriceWithDiscount(price, discount);
+
+    document.getElementById('priceWithDiscount').innerText = "El precio con descuento es " + priceWithDiscount;
+
+}
+
+function getPriceWithDiscount(price, discount) {
+    const percentagePricewithDiscount = 100 - discount;
+    return (price * percentagePricewithDiscount) / 100;
+}
